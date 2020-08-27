e47cdc6bdfca27209fc004d19d5ed0b995268f64
huangweizhen123 <958859946@qq.com>
2020-08-27 17:54:51 +0800

    ss456

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.yys.test.mapper")
+@ComponentScans(value = {@ComponentScan("com.yys.test")})
+@EnableTransactionManagement
 public class DemoApplication {
 
     public static void main(String[] args) {
