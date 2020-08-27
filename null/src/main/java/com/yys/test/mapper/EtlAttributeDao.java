84614c75f095996f585fa6f1521115b45e6f9727
huangweizhen123 <958859946@qq.com>
2020-08-27 17:58:36 +0800

    ss456Matvvvv

---
+++ null/src/main/java/com/yys/test/mapper/EtlAttributeDao.java
@@ -0,0 +1,16 @@
+package com.yys.test.mapper;
+
+import com.yys.test.entity.EtlAttribute;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * etl属性 Mapper 接口
+ * </p>
+ *
+ * @author hwz
+ * @since 2020-08-24
+ */
+public interface EtlAttributeDao extends BaseMapper<EtlAttribute> {
+
+}
